5685546515dced41a2fe051b51edb04efb006919
kor-bim <dbsgksqlschl@gmail.com>
2025-03-17 20:47:02 +0900

    이미지 용량 줄이고 최적화 작업

--- src/app/_components/gallery.tsx
+++ src/app/_components/gallery.tsx
@@ -2,15 +2,16 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
-import { Image } from '@heroui/image'
+import Image from 'next/image'
 import { Icon } from '@iconify/react'
 import 'swiper/css'
+import { StaticImageData } from 'next/image'
 
 export const Gallery = ({
   pictures,
   initialIndex
 }: {
-  pictures: { src: string; alt: string }[]
+  pictures: { src: string | StaticImageData; alt: string }[]
   initialIndex: number
 }) => {
   return (
@@ -34,13 +35,8 @@ export const Gallery = ({
         className="w-screen h-screen flex items-center justify-center"
       >
         {pictures.map((picture, index) => (
-          <SwiperSlide key={index} className="w-full flex flex-col items-center justify-center">
-            <Image
-              src={picture.src}
-              alt={picture.alt}
-              className="w-full h-full object-contain"
-              classNames={{ wrapper: 'w-full h-full !max-w-full' }}
-            />
+          <SwiperSlide key={index} className="w-full !flex flex-col items-center justify-center">
+            <Image src={picture.src} alt={picture.alt} className="w-full h-full object-contain" />
           </SwiperSlide>
         ))}
       </Swiper>
--- src/app/_components/picture.tsx
+++ src/app/_components/picture.tsx
@@ -9,31 +9,57 @@ import { Icon } from '@iconify/react'
 import { Gallery } from './gallery'
 import { motion } from 'framer-motion'
 
+import wedding1 from '@public/wedding/1.jpg'
+import wedding2 from '@public/wedding/2.jpg'
+import wedding3 from '@public/wedding/3.jpg'
+import wedding4 from '@public/wedding/4.jpg'
+import wedding5 from '@public/wedding/5_land.jpg'
+import wedding6 from '@public/wedding/6.jpg'
+import wedding7 from '@public/wedding/7.jpg'
+import wedding8 from '@public/wedding/8.jpg'
+import wedding9 from '@public/wedding/9_land.jpg'
+import wedding10 from '@public/wedding/10_land.jpg'
+import wedding11 from '@public/wedding/11.jpg'
+import wedding12 from '@public/wedding/12.jpg'
+import wedding13 from '@public/wedding/13.jpg'
+import wedding14 from '@public/wedding/14.jpg'
+import wedding15 from '@public/wedding/15.jpg'
+import wedding16 from '@public/wedding/16.jpg'
+import wedding17 from '@public/wedding/17.jpg'
+import wedding18 from '@public/wedding/18.jpg'
+import wedding19 from '@public/wedding/19.jpg'
+import wedding20 from '@public/wedding/20_land.jpg'
+import wedding21 from '@public/wedding/21.jpg'
+import wedding22 from '@public/wedding/22.jpg'
+import wedding23 from '@public/wedding/23.jpg'
+import wedding24 from '@public/wedding/24_land.jpg'
+
 // **이미지 리스트**
 const pictures = [
-  { src: '/main-picture/5_land.webp', alt: '03-1-2_22', width: 1920, height: 1080 },
-  { src: '/main-picture/9_land.webp', alt: '05-2_17', width: 1920, height: 1080 },
-  { src: '/main-picture/1.webp', alt: '01-1_24', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/2.webp', alt: '02-1qq1_21', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/3.webp', alt: '02-1qq2_23', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/4.webp', alt: '03-1-1_20', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/6.webp', alt: '04-1-1_18', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/7.webp', alt: '04-1-2_19', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/8.webp', alt: '05-1_16', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/10_land.webp', alt: '06-1_15', orientation: 'landscape', width: 1920, height: 1080 },
-  { src: '/main-picture/07-1-1_13.webp', alt: '07-1-1_13', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/07-1-2_14.webp', alt: '07-1-1_13', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/08-1-2_12.webp', alt: '08-1-2_12', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/09-1-1_9.webp', alt: '09-1-1_9', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/09-1-2_11.webp', alt: '09-1-2_11', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/10-1-1_7.webp', alt: '10-1-1_7', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/10-1-2_8.webp', alt: '10-1-2_8', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/11-1_5.webp', alt: '11-1_5', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/11-2_6.webp', alt: '11-2_6', orientation: 'landscape', width: 1920, height: 1080 },
-  { src: '/main-picture/12-1-1_2.webp', alt: '12-1-1_2', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/12-1-2_3.webp', alt: '12-1-2_3', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/12-1-3_4.webp', alt: '12-1-3_4', orientation: 'portrait', width: 1080, height: 1920 },
-  { src: '/main-picture/13-1_1.webp', alt: '13-1_1', orientation: 'landscape', width: 1920, height: 1080 }
+  { src: wedding5, alt: 'wedding5', width: 1920, height: 1080, orientation: 'landscape' },
+  { src: wedding9, alt: 'wedding9', width: 1920, height: 1080, orientation: 'landscape' },
+  { src: wedding1, alt: 'wedding1', width: 1080, height: 1920 },
+  { src: wedding2, alt: 'wedding2', width: 1080, height: 1920 },
+  { src: wedding3, alt: 'wedding3', width: 1080, height: 1920 },
+  { src: wedding4, alt: 'wedding4', width: 1080, height: 1920 },
+  { src: wedding6, alt: 'wedding6', width: 1080, height: 1920 },
+  { src: wedding7, alt: 'wedding7', width: 1080, height: 1920 },
+  { src: wedding8, alt: 'wedding8', width: 1080, height: 1920 },
+  { src: wedding10, alt: 'wedding10', width: 1920, height: 1080, orientation: 'landscape' },
+  { src: wedding11, alt: 'wedding11', width: 1080, height: 1920 },
+  { src: wedding12, alt: 'wedding12', width: 1080, height: 1920 },
+  { src: wedding13, alt: 'wedding13', width: 1080, height: 1920 },
+  { src: wedding14, alt: 'wedding14', width: 1080, height: 1920 },
+  { src: wedding15, alt: 'wedding15', width: 1080, height: 1920 },
+  { src: wedding16, alt: 'wedding16', width: 1080, height: 1920 },
+  { src: wedding17, alt: 'wedding17', width: 1080, height: 1920 },
+  { src: wedding18, alt: 'wedding18', width: 1080, height: 1920 },
+  { src: wedding19, alt: 'wedding19', width: 1080, height: 1920 },
+  { src: wedding20, alt: 'wedding20', width: 1920, height: 1080, orientation: 'landscape' },
+  { src: wedding21, alt: 'wedding21', width: 1080, height: 1920 },
+  { src: wedding22, alt: 'wedding22', width: 1080, height: 1920 },
+  { src: wedding23, alt: 'wedding23', width: 1080, height: 1920 },
+  { src: wedding24, alt: 'wedding24', width: 1920, height: 1080, orientation: 'landscape' }
 ]
 
 export const PictureSection = () => {
