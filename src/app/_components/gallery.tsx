8e8d63782378fa27db67f7f13554bdeda7542f7e
kor-bim <dbsgksqlschl@gmail.com>
2025-03-17 18:05:38 +0900

    map 링크 교체

--- src/app/_components/gallery.tsx
+++ src/app/_components/gallery.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
+import { Navigation, Thumbs } from 'swiper/modules'
 import { Image } from '@heroui/image'
 import { Icon } from '@iconify/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import 'swiper/css/thumbs'
 
 export const Gallery = ({
   pictures,
@@ -15,8 +16,10 @@ export const Gallery = ({
   pictures: { src: string; alt: string }[]
   initialIndex: number
 }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+
   return (
-    <div className="relative w-full h-full flex items-center justify-center">
+    <div className="relative w-full h-full flex flex-col items-center justify-center">
       <button className="gallery-prev absolute left-1 z-50">
         <Icon icon="solar:alt-arrow-left-outline" width="24" height="24" />
       </button>
@@ -24,15 +27,13 @@ export const Gallery = ({
         <Icon icon="solar:alt-arrow-right-outline" width="24" height="24" />
       </button>
 
+      {/* Main Swiper */}
       <Swiper
         initialSlide={initialIndex}
-        navigation={{
-          prevEl: '.gallery-prev',
-          nextEl: '.gallery-next'
-        }}
-        pagination={{ clickable: true }}
+        navigation={{ prevEl: '.gallery-prev', nextEl: '.gallery-next' }}
+        thumbs={{ swiper: thumbsSwiper }}
         keyboard={{ enabled: true }}
-        modules={[Navigation]}
+        modules={[Navigation, Thumbs]}
         className="w-screen h-screen flex items-center justify-center"
       >
         {pictures.map((picture, index) => (
@@ -42,6 +43,28 @@ export const Gallery = ({
               alt={picture.alt}
               className="w-full h-full object-contain"
               classNames={{ wrapper: 'w-full h-full !max-w-full' }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Thumbnail Swiper */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[Navigation, Thumbs]}
+        className="w-full max-w-4xl mt-4"
+      >
+        {pictures.map((picture, index) => (
+          <SwiperSlide key={index} className="cursor-pointer">
+            <Image
+              src={picture.src}
+              alt={picture.alt}
+              className="w-full h-24 object-cover"
+              classNames={{ wrapper: 'w-full h-24 !max-w-full' }}
             />
           </SwiperSlide>
         ))}
