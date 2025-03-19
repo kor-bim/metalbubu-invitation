2e3ffbb513d4b9eea8c86588e1f536501b470a91
kor-bim <dbsgksqlschl@gmail.com>
2025-03-19 22:44:40 +0900

    소개 이미지 교체

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    unoptimized: true,
-    minimumCacheTTL: 31536000
+    minimumCacheTTL: 31536000,
+    formats: ['image/webp']
   },
   output: 'standalone'
 }
