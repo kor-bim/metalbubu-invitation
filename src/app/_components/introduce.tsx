ae4c509c9f6365313fa2ffe774b115865ce06fe3
kor-bim <dbsgksqlschl@gmail.com>
2025-03-19 22:07:28 +0900

    소개 이미지 교체

--- src/app/_components/introduce.tsx
+++ src/app/_components/introduce.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import introduceImg from '@public/introduce-2.webp'
+// import Image from 'next/image'
+// import introduceImg from '@public/introduce-2.webp'
 import { Title3Icon } from '@/components/essential'
 
 export const IntroduceSection = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <div className="relative w-full flex flex-col items-center justify-center">
-        <Image src={introduceImg} alt="introduceImg" priority />
+        <img src="/introduce-2.webp" alt="introduceImg" />
+        {/*<Image src={introduceImg} alt="introduceImg" priority />*/}
         <div className="absolute bottom-10 left-[50%] -translate-x-1/2 z-20 w-[70%]">
           <Title3Icon />
         </div>
